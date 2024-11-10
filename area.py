a4b0687958386d302cb1066783d56f55ef9448b8
dajari1 <dajtech88@gmail.com>
2024-11-10 08:02:40 -0500

    my python-intro2

---
+++ area.py
@@ -0,0 +1,13 @@
+#write a python code that will calculate the area of a rectangle of length 10meter and breadth 8meter
+
+l=10
+b=8
+Area=l*b
+print(Area)
+
+#calculate a perimeter of a rectangle
+perimeter=(l+b)*2
+print(perimeter)
+
+d=Area-perimeter
+print(d)
---
+++ userinput.py
@@ -0,0 +1,8 @@
+#write a python code that will collect my details
+print("enter my surname")
+surname=input()
+print("enter my first name")
+firstname=input()
+print("enter my phone number")
+phonenumber=input()
+print("details" , surname , firstname , phonenumber)
